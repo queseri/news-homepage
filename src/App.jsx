05bb02a664e2d6b036d72f346ed8fae2b7337157
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-02-20 05:10:36 +0200

    navigation positioning and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,38 @@ import { useState } from "react";
 import Logo from "./assets/images/logo.svg";
 import Menu from "./assets/images/icon-menu.svg";
 import CloseMenu from "./assets/images/icon-menu-close.svg";
+import Web3Mobile from "./assets/images/image-web-3-mobile.jpg";
+import Web3Desktop from "./assets/images/image-web-3-desktop.jpg";
 import "./sass/main.scss";
 
 function App() {
+  const [menu, setMenu] = useState(false);
+
+  function handleMenu() {
+    setMenu(() => !menu);
+  }
   return (
-    <div className="App font-family">
-      <header>
+    <div className="app font-family">
+      <header className="header flex">
         <div className="container-logo">
           <img src={Logo} alt="" />
         </div>
-        <nav aria-label="main navigation">
+        <nav aria-label="main navigation" className="nav">
           <button
-            className="btn btn-theme-control"
+            className="flex btn btn-menu-control"
             aria-controls="main-nav"
-            aria-expanded="false"
-            onClick={console.log("open and close menu")}
+            aria-expanded={`${menu ? true : false}`}
+            onClick={handleMenu}
           >
-            <img className={`open`} src={Menu} alt="" />
-            <img className={`close`} src={CloseMenu} alt="" />
+            <img className={`${menu ? "close-img" : ""}`} src={Menu} alt="" />
+            <img
+              className={`${menu ? "" : "close-img"}`}
+              src={CloseMenu}
+              alt=""
+            />
           </button>
-          <ul id="main-nav">
+
+          <ul id="main-nav" className={`main-nav ${menu ? "show-nav" : ""}`}>
             <li>
               <a href="">Home</a>
             </li>
@@ -40,7 +52,28 @@ function App() {
           </ul>
         </nav>
       </header>
-      <main>main</main>
+      <main>
+        <div className="hero">
+          <div className="container-hero-img">
+            <picture>
+              <source media="(min-width: 720px)" srcSet={Web3Desktop} />
+              <img src={Web3Mobile} alt="" />
+            </picture>
+          </div>
+          <div className="hero-content">
+            <h1>The Bright Future of Web 3.0?</h1>
+            <p>
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            <a href=""  rel="noopener noreferrer">
+              Read more
+              <span className="sr-only">about web3 technology</span>
+            </a>
+          </div>
+        </div>
+      </main>
       <footer>footer</footer>
     </div>
   );
