272beb70644b60e5cb6c94f8d44b6c0167c873af
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-02-21 05:35:32 +0200

    article setup and layout

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   }
   return (
     <div className="app font-family">
-      <div className={`overlay ${menu ? "show-overlay" : "" }`}></div>
+      <div className={`overlay ${menu ? "show-overlay" : ""}`}></div>
       <header className="header flex">
         <div className="container-logo">
           <img src={Logo} alt="" />
@@ -36,19 +36,29 @@ function App() {
 
           <ul id="main-nav" className={`main-nav ${menu ? "show-nav" : ""}`}>
             <li>
-              <a className="nav-link" href="">Home</a>
+              <a className="nav-link" href="">
+                Home
+              </a>
             </li>
             <li>
-              <a className="nav-link" href="">New</a>
+              <a className="nav-link" href="">
+                New
+              </a>
             </li>
             <li>
-              <a className="nav-link" href="">Popular</a>
+              <a className="nav-link" href="">
+                Popular
+              </a>
             </li>
             <li>
-              <a className="nav-link" href="">Trending</a>
+              <a className="nav-link" href="">
+                Trending
+              </a>
             </li>
             <li>
-              <a className="nav-link" href="">Categories</a>
+              <a className="nav-link" href="">
+                Categories
+              </a>
             </li>
           </ul>
         </nav>
@@ -68,12 +78,35 @@ function App() {
               power of the platforms back into the hands of the people. But is
               it really fulfilling its promise?
             </p>
-            <a className="nav-link" href=""  rel="noopener noreferrer">
+            <a className="nav-link" href="" rel="noopener noreferrer">
               Read more
               <span className="sr-only">about web3 technology</span>
             </a>
           </div>
         </div>
+        <article>
+          <h2>New</h2>
+          <div className="artcles">
+            <div className="article">
+              <h3>Hydrogen VS Electric Cars</h3>
+              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </div>
+            <div className="article">
+              <h3>The Downsides of AI Artistry</h3>
+              <p>
+                What are the possible adverse effects of on-demand AI image
+                generation?
+              </p>
+            </div>
+            <div className="article">
+              <h3>Is VC Funding Drying Up?</h3>
+              <p>
+                Private funding by VC firms is down 50% YOY. We take a look at
+                what that means.
+              </p>
+            </div>
+          </div>
+        </article>
       </main>
       <footer>footer</footer>
     </div>
