32f480306fd526b4d49bd7ce4c928f4c8e104c00
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-02-25 14:18:06 +0200

    aside standalone component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from "react";
-import Retros from "./assets/images/image-retro-pcs.jpg";
-import TopLaptops from "./assets/images/image-top-laptops.jpg";
-import Gaming from "./assets/images/image-gaming-growth.jpg";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import "./sass/main.scss";
 import Article from "./components/Article";
+import Aside from "./components/Aside";
 
 function App() {
   const [menu, setMenu] = useState(false);
@@ -28,60 +26,9 @@ function App() {
 
       <main className="main">
         <Hero />
-       <Article />
+        <Article />
       </main>
-      <aside className="aside">
-        <ul className="aside-list">
-          <li className="aside-item">
-            <div className="aside-img">
-              <img src={Retros} alt="" />
-            </div>
-            <div className="aside-content">
-              <span className="aside-item-num">01</span>
-              <h2 className="aside-item-title">
-                <a className="aside-item-link" href="">
-                  Reviving Retro PCs
-                </a>
-              </h2>
-              <p className="aside-content-text">
-                What happens when old PCs are given modern upgrades?
-              </p>
-            </div>
-          </li>
-          <li className="aside-item">
-            <div className="aside-img">
-              <img src={TopLaptops} alt="" />
-            </div>
-            <div className="aside-content">
-              <span className="aside-item-num">02</span>
-              <h2 className="aside-item-title">
-                <a className="aside-item-link" href="">
-                  Top 10 Laptops of 2022
-                </a>
-              </h2>
-              <p className="aside-content-text">
-                Our best picks for various needs and budgets.
-              </p>
-            </div>
-          </li>
-          <li className="aside-item">
-            <div className="aside-img">
-              <img src={Gaming} alt="" />
-            </div>
-            <div className="aside-content">
-              <span className="aside-item-num">03</span>
-              <h2 className="aside-item-title">
-                <a className="aside-item-link" href="">
-                  The Growth of Gaming
-                </a>
-              </h2>
-              <p className="aside-content-text">
-                How the pandemic has sparked fresh opportunities.
-              </p>
-            </div>
-          </li>
-        </ul>
-      </aside>
+      <Aside />
     </div>
   );
 }
---
+++ src/components/Aside.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Retros from "../assets/images/image-retro-pcs.jpg";
+import TopLaptops from "../assets/images/image-top-laptops.jpg";
+import Gaming from "../assets/images/image-gaming-growth.jpg";
+function Aside() {
+  return (
+    <aside className="aside">
+      <ul className="aside-list">
+        <li className="aside-item">
+          <div className="aside-img">
+            <img src={Retros} alt="" />
+          </div>
+          <div className="aside-content">
+            <span className="aside-item-num">01</span>
+            <h2 className="aside-item-title">
+              <a className="aside-item-link" href="">
+                Reviving Retro PCs
+              </a>
+            </h2>
+            <p className="aside-content-text">
+              What happens when old PCs are given modern upgrades?
+            </p>
+          </div>
+        </li>
+        <li className="aside-item">
+          <div className="aside-img">
+            <img src={TopLaptops} alt="" />
+          </div>
+          <div className="aside-content">
+            <span className="aside-item-num">02</span>
+            <h2 className="aside-item-title">
+              <a className="aside-item-link" href="">
+                Top 10 Laptops of 2022
+              </a>
+            </h2>
+            <p className="aside-content-text">
+              Our best picks for various needs and budgets.
+            </p>
+          </div>
+        </li>
+        <li className="aside-item">
+          <div className="aside-img">
+            <img src={Gaming} alt="" />
+          </div>
+          <div className="aside-content">
+            <span className="aside-item-num">03</span>
+            <h2 className="aside-item-title">
+              <a className="aside-item-link" href="">
+                The Growth of Gaming
+              </a>
+            </h2>
+            <p className="aside-content-text">
+              How the pandemic has sparked fresh opportunities.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Aside;
